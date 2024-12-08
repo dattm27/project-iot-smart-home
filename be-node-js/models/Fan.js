3b4a888bca279c2145d50da4d910332c863b3430
SonProCode <sonchl20182021@gmail.com>
2024-12-08 10:22:31 +0700

    Them Module Quat va hoan thien chuc nang bat tat den va quat

---
+++ be-node-js/models/Fan.js
@@ -0,0 +1,36 @@
+// models/Light.js
+const mongoose = require('mongoose');
+
+const fanSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,  // Tên quạt là duy nhất
+    },
+    status: {
+        type: Number,
+        required: true,
+        enum: [0, 1],  // 0 là tắt, 1 là bật
+        default: 0,  // Mặc định là tắt
+    },
+    room: {
+        type: String,
+        required: true,  // Cần phải có thông tin phòng
+    },
+    // Các trường liên quan đến hẹn giờ
+    timerEnabled: {
+        type: Boolean,
+        default: false,  // Chế độ hẹn giờ mặc định là tắt
+    },
+    autoOnTime: {
+        type: Date,  // Thời gian tự động bật đèn
+    },
+    autoOffTime: {
+        type: Date,  // Thời gian tự động tắt đèn
+    },
+});
+
+// Tạo model từ schema
+const Fan = mongoose.model('Fan', fanSchema);
+
+module.exports = Fan;
--- be-node-js/server.js
+++ be-node-js/server.js
@@ -10,6 +10,7 @@ const app = express();
 const port = 4000;
 const ip = '192.168.1.4';
 const brokerUrl = 'mqtt://192.168.1.4'; // Static IP
+const phoneIp = 'http://192.168.1.25:8080';
 const options = {
     port: 1883,
     //username: 'hivemq.webclient.1732431972503',
@@ -19,16 +20,18 @@ const options = {
     reconnectPeriod: 1000,
     connectTimeout: 30 * 1000,
 };
-const phoneIp = 'http://192.168.1.25:8080';
+
 // Khai báo các topic
 const fireAlarmTopic = 'MQ135/FireAlarm';
 const MQ135StatisticsTopic = 'MQ135/Statistics';
 const LightsControlTopic = 'lights/01';
+const FansControlTopic = 'fans/01';
 
 // Khai báo các model
 const FireAlarm = require('./models/FireAlarm');  // Import model FireAlarm từ thư mục models
 const MQ135Statistics = require('./models/MQ135Statistics');
 const Light = require('./models/Light');
+const Fan = require('./models/Fan');
 
 //Khai báo các biến toàn cục
 let isFire = false;
@@ -149,6 +152,145 @@ app.get('/fire-alarm', (req, res) => {
     res.status(200).json({ isFire: isFire });
 });
 
+////////////////////////////////////////// XU LI LIEN QUAN DEN QUAT //////////////////////////////////////////
+
+// Xử lí tắt bật quạt
+app.put('/fans/OnOff', async (req, res) => {
+    //console.log('BODY', req.body);
+    const { type, name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({ error: 'Cần cung cấp tên quạt (name)' });
+    }
+
+    if (type !== 1 && type !== 0) {
+        return res.status(400).json({ error: 'Tham số "type" phải là 1 (bật) hoặc 0 (tắt)' });
+    }
+
+    try {
+        // Tìm quạt theo name
+        let fan = await Fan.findOne({ name });
+
+        if (!fan) {
+            return res.status(404).json({ error: 'Quạt không tồn tại' });
+        }
+
+        // Cập nhật trạng thái của đèn
+        fan.status = type === 1 ? 1 : 0;
+        await fan.save();
+
+        const message = JSON.stringify({ type });
+
+        // Gửi tin nhắn vào Mosquitto broker tại topic /lights/01
+        mqttClient.publish(FansControlTopic, message, { qos: 0 }, (err) => {
+            if (err) {
+                console.error('Error publishing to MQTT broker:', err);
+                return res.status(500).json({ error: 'Không thể gửi tin nhắn đến Mosquitto broker' });
+            }
+            // Trả về phản hồi thành công
+            res.status(200).json({
+                message: type === 1 ? 'Quạt đã bật' : 'Quạt đã tắt',
+                fanStatus: fan.status
+            });
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: 'Lỗi khi cập nhật trạng thái quạt' });
+    }
+});
+
+// Xử lí thêm quạt mới
+app.post('/fans/', async (req, res) => {
+    const { name, status, room, timerEnabled, autoOnTime, autoOffTime } = req.body;
+
+    if (!name || !room) {
+        return res.status(400).json({ error: 'Cần cung cấp tên quạt (name) và phòng (room)' });
+    }
+
+    try {
+        // Kiểm tra xem quạt đã tồn tại chưa
+        let existingFan = await Fan.findOne({ name });
+        if (existingFan) {
+            return res.status(400).json({ error: 'Quạt với tên này đã tồn tại' });
+        }
+
+        // Tạo một đèn mới
+        const newFan = new Fan({
+            name,
+            room,
+            status: 0,
+            timerEnabled: timerEnabled !== undefined ? timerEnabled : false,
+            autoOnTime: autoOnTime || null,
+            autoOffTime: autoOffTime || null
+        });
+
+        // Lưu đèn mới vào cơ sở dữ liệu
+        await newFan.save();
+
+        // Trả về thông tin đèn đã tạo
+        res.status(201).json({
+            message: 'Quạt mới đã được thêm thành công',
+            Fan: newFan
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Lỗi khi thêm quạt mới' });
+    }
+});
+
+// Lấy danh sách các quạt
+app.get('/fans/', async (req, res) => {
+    try {
+        // Lấy tất cả đèn từ cơ sở dữ liệu
+        const fans = await Fan.find();
+
+        // Kiểm tra nếu danh sách đèn trống
+        if (fans.length === 0) {
+            return res.status(404).json({ error: 'Không có quạt nào trong cơ sở dữ liệu' });
+        }
+
+        // Trả về danh sách đèn
+        res.status(200).json({
+            message: 'Danh sách đèn đã được lấy thành công',
+            fans,
+        });
+    } catch (error) {
+        console.error('Error fetching fans:', error);
+        res.status(500).json({ error: 'Lỗi khi lấy danh sách quạt' });
+    }
+});
+
+// Xóa quạt theo tên
+app.delete('/fans/', async (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({ error: 'Cần cung cấp tên đèn để xóa' });
+    }
+
+    try {
+        // Tìm đèn theo tên
+        const fan = await Fan.findOne({ name });
+        if (!fan) {
+            return res.status(404).json({ error: 'Không tìm thấy quạt với tên này' });
+        }
+
+        // Xóa đèn
+        await Fan.deleteOne({ name });
+
+        // Trả về phản hồi thành công
+        res.status(200).json({
+            message: 'Quạt đã được xóa thành công',
+            fan,
+        });
+    } catch (error) {
+        console.error('Error deleting fan by name:', error);
+        res.status(500).json({ error: 'Lỗi khi xóa quạt' });
+    }
+});
+
+
+////////////////////////////////////////// XU LI LIEN QUAN DEN DEN //////////////////////////////////////////
+
 // Xử lí tắt bật đèn
 app.put('/lights/OnOff', async (req, res) => {
     //console.log('BODY', req.body);
@@ -193,6 +335,7 @@ app.put('/lights/OnOff', async (req, res) => {
         res.status(500).json({ error: 'Lỗi khi cập nhật trạng thái đèn' });
     }
 });
+
 // Xử lí thay đổi chức năng hẹn giờ cho đèn
 app.put('/lights/Timer/', async (req, res) => {
     const { name, timerEnabled, autoOnTime, autoOffTime } = req.body;
@@ -243,7 +386,7 @@ app.put('/lights/Timer/', async (req, res) => {
 
 // Xử lí thêm đèn mới
 app.post('/lights/', async (req, res) => {
-    const { name, status, room } = req.body;
+    const { name, status, room, timerEnabled, autoOnTime, autoOffTime } = req.body;
 
     if (!name || !room) {
         return res.status(400).json({ error: 'Cần cung cấp tên đèn (name) và phòng (room)' });
@@ -260,7 +403,10 @@ app.post('/lights/', async (req, res) => {
         const newLight = new Light({
             name,
             room,
-            status: 0  // Mặc định là tắt khi tạo mới
+            status: 0,
+            timerEnabled: timerEnabled !== undefined ? timerEnabled : false,
+            autoOnTime: autoOnTime || null,
+            autoOffTime: autoOffTime || null,
         });
 
         // Lưu đèn mới vào cơ sở dữ liệu
@@ -276,6 +422,56 @@ app.post('/lights/', async (req, res) => {
     }
 });
 
+// Lấy danh sách các đèn
+app.get('/lights/', async (req, res) => {
+    try {
+        // Lấy tất cả đèn từ cơ sở dữ liệu
+        const lights = await Light.find();
+
+        // Kiểm tra nếu danh sách đèn trống
+        if (lights.length === 0) {
+            return res.status(404).json({ error: 'Không có đèn nào trong cơ sở dữ liệu' });
+        }
+
+        // Trả về danh sách đèn
+        res.status(200).json({
+            message: 'Danh sách đèn đã được lấy thành công',
+            lights,
+        });
+    } catch (error) {
+        console.error('Error fetching lights:', error);
+        res.status(500).json({ error: 'Lỗi khi lấy danh sách đèn' });
+    }
+});
+
+// Xóa đèn theo tên
+app.delete('/lights/', async (req, res) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).json({ error: 'Cần cung cấp tên đèn để xóa' });
+    }
+
+    try {
+        // Tìm đèn theo tên
+        const light = await Light.findOne({ name });
+        if (!light) {
+            return res.status(404).json({ error: 'Không tìm thấy đèn với tên này' });
+        }
+
+        // Xóa đèn
+        await Light.deleteOne({ name });
+
+        // Trả về phản hồi thành công
+        res.status(200).json({
+            message: 'Đèn đã được xóa thành công',
+            light,
+        });
+    } catch (error) {
+        console.error('Error deleting light by name:', error);
+        res.status(500).json({ error: 'Lỗi khi xóa đèn' });
+    }
+});
 
 // Hàm kiểm tra và tự động bật/tắt đèn theo thời gian
 const checkAutoLights = async () => {
@@ -319,8 +515,6 @@ const checkAutoLights = async () => {
 
 // Thiết lập một chu kỳ để kiểm tra mỗi phút (60000ms)
 setInterval(checkAutoLights, 60000);
-
-
 
 app.listen(port, () => {
     console.log(`Server đang chạy tại http://${ip}:${port}`);
